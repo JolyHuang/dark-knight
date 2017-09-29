b12f98d9b1c5204c963708a8a02e02db627ea4f2
JolyHuang <JolyEmail@Gmail.COM>
2017-09-29 15:23:05 +0800

    delete

--- com.sharingif.cube.dark.knight.view/src/app/theme.switcher.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-theme-switcher',
-  templateUrl: './theme.switcher.component.html',
-})
-export class ThemeSwitcherComponent {
-
-}
