8a85fd7f877d8308ce3ae67e2f80034bae9eff88
JolyHuang <JolyEmail@Gmail.COM>
2017-10-11 16:54:19 +0800

    init

--- com.sharingif.cube.dark.knight.view/src/app/http/http.json.service.ts
+++ com.sharingif.cube.dark.knight.view/src/app/http/http.json.service.ts
@@ -1,5 +1,62 @@
 import { Injectable } from '@angular/core';
+import { HttpHeaders, HttpClient } from '@angular/common/http';
+
+
+const headers = new HttpHeaders().set("Content-Type", "application/json");
 
 @Injectable()
-export class HeroService {
+export class HttpJsonService {
+
+  constructor(
+    private http: HttpClient
+  ) {};
+
+  ip: string = "127.0.0.1";
+  port: number = 9300;
+  address: string;
+  useHttps: boolean;
+  contextPath: string;
+
+
+  getUrlPrefix(): string {
+    if(this.address !== null) {
+      if(this.useHttps) {
+        return "https://"+this.address+"/"+this.contextPath;
+      } else {
+        return "http://"+this.address+"/"+this.contextPath;
+      }
+    }
+
+    if(this.useHttps) {
+      return "https://"+this.ip+":"+this.port+"/"+this.contextPath;
+    } else {
+      return "http://"+this.ip+":"+this.port+"/"+this.contextPath;
+    }
+  }
+
+  get(url: string): any {
+    this.http
+      .get(this.getUrlPrefix()+url, {headers})
+      .subscribe(
+        res => {
+          return res["_data"];
+        },
+        err => {
+          console.log("Error occured");
+        });
+  }
+
+  post(url: string, data: any): any {
+    this.http
+      .post(this.getUrlPrefix()+url, data,{headers})
+      .subscribe(
+        res => {
+          return res["_data"];
+        },
+        err => {
+          console.log("Error occured");
+        });
+  }
+
+
 }
