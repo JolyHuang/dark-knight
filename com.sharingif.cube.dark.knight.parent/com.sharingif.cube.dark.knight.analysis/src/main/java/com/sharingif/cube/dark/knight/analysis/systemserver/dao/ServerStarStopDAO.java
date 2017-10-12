a786eba139a1d7cbaf04dec662a9bffe14c23d84
JolyHuang <JolyEmail@Gmail.COM>
2017-10-12 10:29:36 +0800

    init

---
+++ com.sharingif.cube.dark.knight.parent/com.sharingif.cube.dark.knight.analysis/src/main/java/com/sharingif/cube/dark/knight/analysis/systemserver/dao/ServerStarStopDAO.java
@@ -0,0 +1,12 @@
+package com.sharingif.cube.dark.knight.analysis.systemserver.dao;
+
+/**
+ * 系统启停
+ *
+ * @author Joly
+ * @version v1.0
+ * @since v1.0
+ * 2017/10/11 下午7:55
+ */
+public interface ServerStarStopDAO {
+}
---
+++ com.sharingif.cube.dark.knight.parent/com.sharingif.cube.dark.knight.analysis/src/main/java/com/sharingif/cube/dark/knight/analysis/systemserver/dao/impl/ServerStarStopDAOImpl.java
@@ -0,0 +1,23 @@
+package com.sharingif.cube.dark.knight.analysis.systemserver.dao.impl;
+
+import com.mongodb.client.MongoCollection;
+import com.sharingif.cube.dark.knight.analysis.app.dao.impl.CubeMongoDBDAOImpl;
+import com.sharingif.cube.dark.knight.analysis.systemserver.dao.ServerStarStopDAO;
+import org.bson.Document;
+
+/**
+ * 系统启停
+ *
+ * @author Joly
+ * @version v1.0
+ * @since v1.0
+ * 2017/10/11 下午7:56
+ */
+public class ServerStarStopDAOImpl extends CubeMongoDBDAOImpl implements ServerStarStopDAO {
+
+    @Override
+    protected MongoCollection<Document> getCollection() {
+        return null;
+    }
+
+}
