89ef7622e7570a89c3fb408e0378beb2a9c8e0cd
JolyHuang <JolyEmail@Gmail.COM>
2017-10-18 12:48:54 +0800

    init

--- com.sharingif.cube.dark.knight.parent/com.sharingif.cube.dark.knight.analysis/src/main/java/com/sharingif/cube/dark/knight/analysis/transaction/service/impl/TransactionServiceImpl.java
+++ com.sharingif.cube.dark.knight.parent/com.sharingif.cube.dark.knight.analysis/src/main/java/com/sharingif/cube/dark/knight/analysis/transaction/service/impl/TransactionServiceImpl.java
@@ -100,24 +100,28 @@ public class TransactionServiceImpl implements TransactionService {
         registerTransaction.setStartTimeBegin(currentDateBegin);
         registerTransaction.setStartTimeEnd(currentDateEnd);
         registerTransaction.setTransId(Transaction.REGISTER);
+        registerTransaction.setTransType(Transaction.TRANSACTION_ERROR);
         long register = transactionDAO.queryCount(registerTransaction);
 
         Transaction rechargeSubmitTransaction = new Transaction();
         rechargeSubmitTransaction.setStartTimeBegin(currentDateBegin);
         rechargeSubmitTransaction.setStartTimeEnd(currentDateEnd);
         rechargeSubmitTransaction.setTransId(Transaction.RECHARGE_SUBMIT);
+        rechargeSubmitTransaction.setTransType(Transaction.TRANSACTION_ERROR);
         long rechargeSubmit = transactionDAO.queryCount(rechargeSubmitTransaction);
 
         Transaction withdrawSubmitTransaction = new Transaction();
         withdrawSubmitTransaction.setStartTimeBegin(currentDateBegin);
         withdrawSubmitTransaction.setStartTimeEnd(currentDateEnd);
         withdrawSubmitTransaction.setTransId(Transaction.WITHDRAW_SUBMIT);
+        withdrawSubmitTransaction.setTransType(Transaction.TRANSACTION_ERROR);
         long withdrawSubmit = transactionDAO.queryCount(withdrawSubmitTransaction);
 
         Transaction loanApplyTransaction = new Transaction();
         loanApplyTransaction.setStartTimeBegin(currentDateBegin);
         loanApplyTransaction.setStartTimeEnd(currentDateEnd);
         loanApplyTransaction.setTransId(Transaction.LOAN_APPLY);
+        loanApplyTransaction.setTransType(Transaction.TRANSACTION_ERROR);
         long loanApply = transactionDAO.queryCount(loanApplyTransaction);
 
         TransactionDay transactionDay = new TransactionDay();
